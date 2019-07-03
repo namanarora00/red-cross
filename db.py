4b39f38a3c9af6aa5fc861dd9b255d713e77b76e
Naman Arora <xavier.arora@gmail.com>
2019-07-03 20:38:48 +0530

    refactor

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 from meta import Table
 
+TABLES = []
+
 
 item = Table("item")
 beneficiery = Table("beneficiery")
 distribution = Table("distribution")
 distributed = Table("distributed")
+admin = Table("admin")
+
 
 item.init_table(["id", "name", "type", "quantity"])
 # items are stored as as string of comma separated values in both tables
@@ -12,3 +16,11 @@ item.init_table(["id", "name", "type", "quantity"])
 
 distribution.init_table(["id", "items", "timestamp"])
 distributed.init_table(["bid", "item", "id", "timestamp", "quantity"])
+admin.init_table(["id"])
+
+
+TABLES.append(beneficiery)
+TABLES.append(item)
+TABLES.append(distributed)
+TABLES.append(distribution)
+TABLES.append(admin)
